ad6b55578dfa6e9c0641638f4ba8a6bc9c078aa7
sushant097 <sushant1234gautam@gmail.com>
2023-06-02 21:25:02 +0545

    Update the code files

--- Session5/utils.py
+++ Session5/utils.py
@@ -2,54 +2,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-
-# Code Block: 3
-# Train data transformations
-train_transforms = transforms.Compose([
-    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
-    transforms.Resize((28, 28)),
-    transforms.RandomRotation((-15., 15.), fill=0),
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,)),
-    ])
-
-# Test data transformations
-test_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-    ])
-
-# Code Block: 4
-train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
-test_data = datasets.MNIST('../data', train=False, download=True, transform=test_transforms)
-
-# Code Block: 5
-batch_size = 512
-
-kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}
-
-train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
-test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
-
-# Code Block 6:
-
-def plot_data_batch():
-  batch_data, batch_label = next(iter(train_loader)) 
-  fig = plt.figure()
-  for i in range(12):
-    plt.subplot(3,4,i+1)
-    plt.tight_layout()
-    plt.imshow(batch_data[i].squeeze(0), cmap='gray')
-    plt.title(batch_label[i].item())
-    plt.xticks([])
-    plt.yticks([])
-  plt.show()
-    
+ 
     
 # Code Block: 8
 
